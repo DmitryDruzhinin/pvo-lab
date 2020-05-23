69ababec66bb5c0761cc41d8ba0a95759fffab84
DmitryDruzhinin <dryzhin@mail.ru>
2020-05-23 22:52:19 +0300

    +

--- анализ качества кода/dirty.py
+++ анализ качества кода/dirty.py
@@ -1,3 +1,4 @@
+"""Dirty"""
 import random
 import numpy as np
 
@@ -5,23 +6,28 @@ DEFAULT_ACCURACY = 3
 
 
 def sum_two_values(first, second):
+    """Returns sum."""
     return first + second
 
 
 def div(x_val, y_val, accuracy):
+    """Rounds numbers."""
     return round(x_val / y_val, accuracy)
 
 
 def get_rand():
+    """Returns random integer."""
     return random.randint(1, 10)
 
 
 def rand_array():
+    """Returns list of random integers."""
     arr = [get_rand(), get_rand(), get_rand()]
     return np.array(arr)
 
 
 def main():
+    """Main function."""
 
 
 main()
