156962207270c5f22e1074dcf7b4774630abd369
DmitryDruzhinin <dryzhin@mail.ru>
2020-05-23 18:46:01 +0300

    +

--- кортежи, списки, словари/1.py
+++ кортежи, списки, словари/1.py
@@ -1,7 +1,6 @@
-list = [10, 20, 30, 40, 50, 60, 70, 80]
-for i, num in enumerate(list):
-    if(i == 0):
+a = [10, 20, 30, 40, 50, 60, 70, 80]
+for i in range(len(a)):
+    if i == 0:
         continue
-    if(i % 2 == 0):
-        print(list[i])
-
+    elif i % 2 == 0:
+        print(a[i])
