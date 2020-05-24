6aa4c596f93a7548fcf94777d59124cb9885fee2
DmitryDruzhinin <dryzhin@mail.ru>
2020-05-24 04:37:46 +0300

    +

--- экзамен/3.py
+++ экзамен/3.py
@@ -1,24 +1,9 @@
-﻿def do_parentheses_match(input_string):
-    s = []
-    balanced = True
-    index = 0
-    while index < len(input_string) and balanced:
-        token = input_string[index]
-        if token == "(":
-            s.append(token)
-        elif token == ")":
-            if len(s) == 0:
-                balanced = False
-            else:
-                s.pop()
-
-        index += 1
-
-    return balanced and len(s) == 0
-
-
-bracket_string = input()
-if(do_parentheses_match(bracket_string)):
-    print('сбалансированы')
-else:
-    print('не сбалансированы')
+﻿def check(my_str):
+    brack = ['()', '{}', '[]']
+    while any(x in my_str for x in brack):
+        for br in brack:
+            my_str = my_str.replace(br, '')
+    return not my_str
+str = input()
+print(str, "-", "balanced"
+if check(str) else "unbalanced")
