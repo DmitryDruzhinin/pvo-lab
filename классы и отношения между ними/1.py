40b00ddf3a847e3bd6a97aa9de15a9d97a723691
DmitryDruzhinin <dryzhin@mail.ru>
2020-05-24 06:05:45 +0300

    +

--- классы и отношения между ними/1.py
+++ классы и отношения между ними/1.py
@@ -1,8 +1,11 @@
 class Loco:
     def __init__(self):
         self._loco = 'oooTT'
+
     def __repr__(self):
         return self._loco
+
+
 class Train:
     def __init__(self, weight):
         self._wagons = []
@@ -10,57 +13,87 @@ class Train:
         self._weight = weight
         self._loco = None
         self.type = 'default'
+
     def add_loco(self):
         self._loco = Loco()
+
     def add_wagon(self, wagon_type):
         self._wagons.append(wagon_type)
+
     def run(self):
-        if (self._weight <= self._max_weigth):
+        if self._weight <= self._max_weigth:
             return True
         else:
             return False
+
     def __repr__(self):
         train = str(self._loco)
         for wagon in self._wagons:
             train += str(wagon)
         return train
+
+
 class TrainPass(Train):
     def __init__(self, _weight):
         super().__init__(_weight)
         self.type = 'passanger'
+
+
 class TrainCargo(Train):
     def __init__(self, _weight):
         super().__init__(_weight)
         self.type = 'cargo'
-class Wagon():
+
+
+class Wagon:
     def __init__(self):
         self.type = None
+
+
 class WagonPass(Wagon):
     def __init__(self, place=''):
+        super().__init__()
         self.place = place
         self.type = 'passenger'
+
     def __repr__(self):
         return ".[_ _]"
+
+
 class WagonCargo(Wagon):
     def __init__(self):
+        super().__init__()
         self.type = 'cargo'
+
     def __repr__(self):
         return ".(_ _)"
+
+
 class WagonPassCoupe(WagonPass):
     def __init__(self, place=''):
         super().__init__(place)
         self.comf_type = 'coupe'
+
+
 class WagonPassPlat(WagonPass):
     def __init__(self, place=''):
         super().__init__(place)
         self.comf_type = 'plat'
+
+
 class WagonPassSit(WagonPass):
     def __init__(self, place=''):
         super().__init__(place)
         self.comf_type = 'sit'
-t = Train(16384)
+
+
+t = Train(int(input ("enter train mass: ")))
 t.add_loco()
 t.add_wagon(WagonPass())
 t.add_wagon(WagonCargo())
 print(t)
-print(t.run())
+ok = t.run()
+if ok:
+    print("ok")
+else:
+    print("can not run, too big mass")
